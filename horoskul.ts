affb4d1c56d233240603328a3b99d52cdf1e5887
Michael Timbs <mtimbs91@gmail.com>
2020-08-26 00:23:47 +0930

    extract functionality and clean up types

--- horoskul.ts
+++ horoskul.ts
@@ -14,23 +14,25 @@ world[3][4] = 1;
 world[4][3] = 1;
 world[4][4] = 1;
 
-
+type Grid = number[][]
 const ALIVE = 1
 const DEAD = 0
 
 const arraySum = (a: number[]): number => a.reduce((a,b) => a+b, 0)
 
-const gridSum = (xs: number[][], xi: number, yi: number): number => [
-  ...xs.slice(Math.max(xi-1, 0), xi+2)
-    .map(ys => ys.slice(Math.max(yi-1, 0), yi+2))
-].reduce((total: number,ys: number[]) => total + arraySum(ys), 0)
+const gridSum = (grid: Grid): number => grid.reduce((total: number,ys: number[]) => total + arraySum(ys), 0)
 
-export const nextGen = (world: number[][]): number[][] =>
-  world.map((ys: number[], xi: number, xs: number[][]) =>
+const subGrid = ({x, y}, grid: Grid): Grid => [
+  ...grid.slice(Math.max(x-1, 0), x+2)
+    .map(ys => ys.slice(Math.max(y-1, 0), y+2))
+]
+
+export const nextGen = (world: Grid): Grid =>
+  world.map((ys: number[], xi: number, xs: Grid) =>
     ys.map((y: number, yi: number) => y === ALIVE
-      ? [2,3].includes(gridSum(xs, xi, yi) - 1)
+      ? [2,3].includes(gridSum(subGrid({x: xi, y: yi}, xs)) - 1)
         ? ALIVE : DEAD
-      : gridSum(xs, xi, yi) === 3
+      : gridSum(subGrid({x: xi, y: yi}, xs)) === 3
         ? ALIVE : DEAD
     )
   )
